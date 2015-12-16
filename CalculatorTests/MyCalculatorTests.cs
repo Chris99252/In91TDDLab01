6fee1b3e10c4a4ab4ce5eb5fe6d408978fa0f7eb
chris <s86610000@gmail.com>
2015-12-17 00:02:39 +0800

    commit Step1. 利用 Visual Studio 幫你產生 TestProjet。

---
+++ CalculatorTests/MyCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class MyCalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
