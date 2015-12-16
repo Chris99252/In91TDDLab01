0eede8e0c881fcdb4f9b9ccabc91049fc185a7f6
chris <s86610000@gmail.com>
2015-12-17 00:20:18 +0800

    commit Step3. 實際寫測試（符合3A原則）。

--- CalculatorTests/MyCalculatorTests.cs
+++ CalculatorTests/MyCalculatorTests.cs
@@ -14,13 +14,17 @@ namespace Calculator.Tests
         [TestMethod()]
         public void AddTest_first_1_second_2_should_be_3()
         {
-            //// Arrage
+            //// Arrange 初始化
+            var target = new MyCalculator(); //// 測試目標叫 target
+            var first = 1; //// 參數名稱跟命名一樣 first
+            var second = 2; //// 參數名稱跟命名一樣 second
+            var expected = 3; //// expected 你期望的
 
-            //// Act
+            //// Act 驗證什麼行為
+            var actual = target.Add(first, second); //// actual 實際得到的
 
-            //// Assert
-
-            Assert.Fail();
+            //// Assert 符合預期
+            Assert.AreEqual(expected, actual); //// 測試：期望的跟你實際得到的一樣
         }
     }
 }
